8c81ff5ca095a7866963be095013ec218a31189c
Your Name <you@example.com>
2026-03-02 21:05:11 +0800

    fix: update import path for Toggle component to include file extension

--- kanban/src/App.jsx
+++ kanban/src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./features/header/Header";
 import { useRef, useState } from "react";
-import Toggle from "./features/toggle/Toggle";
+import Toggle from "./features/toggle/Toggle.jsx";
 import Board from "../src/features/board/Board";
 
 import "./app.css";
